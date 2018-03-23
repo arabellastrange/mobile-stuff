5773c5357aee90a793a4581554e9dd04c88b1429
Lauren Higgins CS2015 <lauren.higgins.2015@uni.strath.ac.uk>
2018-03-23 18:09:42 +0000

    Statements not executing, but now connecting properly to server

--- mappy-travels/SQLConfig.php
+++ mappy-travels/SQLConfig.php
@@ -5,11 +5,11 @@
  * Date: 23/03/2018
  * Time: 12:57
  */
-define('server', 'devweb2017.cis.strath.ac.uk');
-define('un', 'mad3_p');
-define('pw', 'xahJi7kua8Ah');
-$db = mysqli_connect(server,un,pw);
-if($db === false){
+$host = "devweb2017.cis.strath.ac.uk";
+$user = "mad3_p";
+$pass = "xahJi7kua8Ah";
+$dbname = "mad3_p";
+$conn = new mysqli($host,$user,$pass,$dbname);
+if($conn -> connect_error){
     die("ERROR: Unable to connect. " . mysqli_connect_error());
 }
-?>
--- mappy-travels/registration.php
+++ mappy-travels/registration.php
@@ -8,31 +8,74 @@
 require_once 'SQLConfig.php';
 session_start();
 $username = $password = $confirm_password = "";
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if($_SERVER["REQUEST_METHOD"] == "POST") {
     //Username validation
-    if(empty(trim($_POST["username"]))){
+    if (empty(trim($_POST["username"]))) {
         //Some kind of username error;
-    }
-    else{
+    } else {
         //Prepared sql statements
-        $sql = "SELECT id FROM MappyUsers WHERE UserID = ?";
-        if($statement = $mysqli->prepare($sql)){
+        $sql = "SELECT UserID FROM MappyUsers WHERE UserID=?";
+
+        if($stmt = $conn->prepare($sql)){
             //binds parameters to the prepared statement
-            $statement->bind_param("s", $param_username);
+            $stmt->bind_param("s", $param_username);
             //sets the parameters
             $param_username = trim($_POST["username"]);
-            if($statement->execute()){
+            if ($stmt->execute()) {
                 //store result
-                $statement->store_result();
-                if($statement->num_rows == 1){
-                    //some kind of username error
-                }
-                else {
+                $stmt->store_result();
+                if ($stmt->num_rows > 0) {
+                    //some kind of username error - already taken;
+                } else {
                     $username = trim($_POST["username"]);
                 }
+            } else {
+                //Something went wrong
             }
+            //close statement
+            $stmt->close();
+        }
+
+    }
+
+    //Password validation
+    if (empty(trim($_POST['password']))) {
+        //Password error - empty
+    } else {
+        $password = trim($_POST['password']);
+    }
+
+    //Confirm password validation
+    if (empty(trim($_POST["confirmPassword"]))) {
+        //confirm password error - empty
+    } else {
+        $confirmPassword = trim($_POST['confirmPassword']);
+        if ($password != $confirmPassword) {
+            //confirm password error - didn't match
         }
     }
+
+    //REQUIRED HERE: error checking before database insertion
+
+    //prepare insert statement
+    $sql = "INSERT INTO MappyUsers (UserID, UserPW) VALUES (?, ?)";
+    if ($stmt = $conn->prepare($sql)) {
+        //Bind variables to prepared statement as parameters
+        $stmt->bind_param("ss", $param_username, $param_password);
+        //set parameters
+        $param_username = $username;
+        $param_password = password_hash($password, PASSWORD_BCRYPT); //hashes passsword - safety
+
+        if ($stmt->execute()) {
+            echo "successfully made an account, check the database"; //Redirects to homescreen later - just for testing.
+        } else {
+            echo "Something went wrong.";
+        }
+    }
+
+    $stmt->close();
+
+    $conn->close();
 }
 ?>
 <!DOCTYPE html>
@@ -51,7 +94,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <label>Password: </label>
         <input type="password" name="password"/> <br>
         <label>Re-enter Password: </label>
-        <input type="password" name="confirm-password"/> <br>
+        <input type="password" name="confirmPassword"/> <br>
         <input type="submit" value="Submit">
         <p>Already have an account? Sign in</p> <!--needs link to log in page-->
     </form>
