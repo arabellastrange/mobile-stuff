425f74c8515e4a911af42d3dd17fea4299a95f47
Gordon Mason CS2015 <gordon.mason.2015@uni.strath.ac.uk>
2018-03-26 09:54:37 +0100

    fixed a couple of small things and added the files this time -_-

---
+++ mappy-travels/php/testForm.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: losti
+ * Date: 26/03/2018
+ * Time: 09:29
+ */
+echo "Test Successful";
+
+var_dump($_POST);
+
+echo " **** ";
+
+var_dump($_REQUEST);
